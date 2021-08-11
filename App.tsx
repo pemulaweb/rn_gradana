708b29589e7de2acbc86ddd2a61d6c2b3472ecd3
babaiyu <bayuputra9a@gmail.com>
2021-08-11 13:30:44 +0000

    Integrate with react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
 import Routes from './src/routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <PaperProvider>
+      <Routes />
+    </PaperProvider>
+  );
 }
