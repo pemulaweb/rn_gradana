f614e06cebcca4941943920ffd35cae7ed7929b7
babaiyu <bayuputra9a@gmail.com>
2021-08-11 15:07:06 +0000

    Integrate with useList, create list jokes using Flatlist

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useCallback} from 'react';
+import {SafeAreaView, StyleSheet, FlatList, RefreshControl} from 'react-native';
 import {Text} from 'react-native-paper';
+import useList from '../hooks/useList';
+import MyCard from '../components/MyCard';
+import Loading from '../components/Loading';
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text>Ini Home</Text>
-    </View>
-  );
+  const {loading, error, data, onGetList} = useList();
+
+  // Flatlist
+  const renderItem = useCallback(({item}: any) => <MyCard {...item} />, []);
+  const keyExt = useCallback((i: any, index: number) => index.toString(), []);
+
+  if (loading) return <Loading />;
+
+  if (error) return <Text>Error</Text>;
+  else
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={keyExt}
+          maxToRenderPerBatch={8}
+          windowSize={10}
+          refreshControl={
+            <RefreshControl onRefresh={onGetList} refreshing={false} />
+          }
+          contentContainerStyle={{paddingHorizontal: 20}}
+        />
+      </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
