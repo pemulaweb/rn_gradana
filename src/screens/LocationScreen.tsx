733e361b1806ac0d1881e2fa77383a58ce6b2864
babaiyu <bayuputra9a@gmail.com>
2021-08-13 13:40:30 +0000

    Screen for getting location

---
+++ src/screens/LocationScreen.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Text, List, Button} from 'react-native-paper';
+import useLocation from '../hooks/useLocation';
+
+export default function LocationScreen() {
+  const {onGetLocation, location, error} = useLocation();
+
+  if (error)
+    return (
+      <SafeAreaView>
+        <Text>
+          There is an error when getting location. Please check the permissions.
+        </Text>
+      </SafeAreaView>
+    );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <List.Section>
+        <List.Subheader>Your Current Location</List.Subheader>
+        <List.Item title={`Latitude: ${location?.coords?.latitude}`} />
+        <List.Item title={`Longitude : ${location?.coords?.longitude}`} />
+        <List.Item title={`Accuracy: ${location?.coords?.accuracy}`} />
+      </List.Section>
+
+      <Button mode="contained" icon="google-maps" onPress={onGetLocation}>
+        GET LOCATION
+      </Button>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
