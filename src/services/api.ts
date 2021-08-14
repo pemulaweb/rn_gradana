9e7bf72268ba6aba6552be16f9983ab8f6a2b8ef
babaiyu <bayuputra9a@gmail.com>
2021-08-14 17:41:03 +0000

    Add api wilayah

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,28 @@
 import axios from 'axios';
 import config from '../config';
 
-const {baseUrl} = config;
+const {baseUrl, wilayahUrl} = config;
 
 // Get Random Jokes
 export const apiGetRandomJokes = async () => {
   const url = `${baseUrl}/random_ten`;
+  return await axios.get(url);
+};
+
+// Province
+export const apiGetProvince = async () => {
+  const url = `${wilayahUrl}/provinces.json`;
+  return await axios.get(url);
+};
+
+// Regencies
+export const apiGetRegencies = async (id: number) => {
+  const url = `${wilayahUrl}/regencies/${id}.json`;
+  return await axios.get(url);
+};
+
+// Districts
+export const apiGetDistricts = async (id: number) => {
+  const url = `${wilayahUrl}/districts/${id}.json`;
   return await axios.get(url);
 };
